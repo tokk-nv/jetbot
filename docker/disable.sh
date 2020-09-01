716f5475fcbb3c3cc559229dd78f5537c9232b07
Chitoku YATO <cyato@nvidia.com>
2020-09-01 13:47:28 -0400

    Make OpenCV camera back default

--- docker/disable.sh
+++ docker/disable.sh
@@ -1,5 +1,5 @@
-sudo docker stop jetbot_camera 
-sudo docker rm jetbot_camera
+#sudo docker stop jetbot_camera 
+#sudo docker rm jetbot_camera
 
 sudo docker stop jetbot_jupyter 
 sudo docker rm jetbot_jupyter
--- docker/enable.sh
+++ docker/enable.sh
@@ -3,5 +3,5 @@ export JETBOT_VERSION=jp44
 JUPYTER_WORKSPACE=${1:-$HOME}  # default to $HOME
 
 ./display/enable.sh
-./camera/enable.sh
+#./camera/enable.sh
 ./jupyter/enable.sh $JUPYTER_WORKSPACE
